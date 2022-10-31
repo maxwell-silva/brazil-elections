666d6b03cbc143a65adec614880e5def6f78dc37
maxwellpereira <maxwell.tj@hotmail.com>
2022-10-31 02:29:08 -0300

    poller implement

---
+++ src/shared/utils/init.mjs
@@ -0,0 +1,16 @@
+import Synchronizer from "../../modules/c0001-president/observers/synchronizer.mjs";
+import tse from "../../config/tse.mjs";
+
+const syncher = new Synchronizer();
+
+syncher.update({
+  kind: "config",
+  uf: "br",
+  file: "br-c0001-e000545-v.json",
+});
+
+syncher.update({
+  kind: "config",
+  uf: "br",
+  file: "br-c0001-e000545-v.json",
+});
